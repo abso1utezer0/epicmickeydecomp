7d0c6452be4cc54aa94dc62e4a21a216aede8c00
Ryan Koop <ryanckoop1@outlook.com>
2022-06-08 20:26:16 -0500

    demo files

--- Environments/_Shared/Scripts/Demo_GV_isle2.lua
+++ Environments/_Shared/Scripts/Demo_GV_isle2.lua
@@ -1,5 +1,6 @@
-local L0_0, L1_1
-function L0_0()
+-- Fully reworked
+
+function demo_gv_isle3_oswaldportal2()
   RestrictCutSceneInput()
   GrabCamera("demo_gv_isle3.OswaldCameraControlPoint 01", "demo_gv_isle3.OswaldMarker1 02", CAMERA_LERP_TRANSITION, 0.25)
   Bark(GetPlayer(), "There he is again. Seems like he really wants you to follow him.", 4, "Gus", "Gus:")
@@ -26,19 +27,17 @@ function L0_0()
   UnrestrictCutSceneInput()
   DestroyEntity("demo_gv_isle3.AI_Oswald 01")
 end
-demo_gv_isle3_oswaldportal2 = L0_0
-function L0_0(A0_2, A1_3)
-  AnimGBReset(A0_2)
-  AnimGBSequence(A0_2, "Appear")
-  Enable(A1_3)
+
+function demo_gv_isle2_EnableSpawners(param1, param2)
+  AnimGBReset(param1)
+  AnimGBSequence(param1, "Appear")
+  Enable(param2)
 end
-demo_gv_isle2_EnableSpawners = L0_0
-function L0_0()
-  local L0_4
-  L0_4 = "demo_gv_isle3.OswaldCameraControlPoint 01"
+
+function demo_gv_isle3_IGC_BunnyEatSpatter()
   RestrictCutSceneInput()
-  SetTransformation(L0_4, vector4(90.9, 18.5, -13), vector4(0, 0, 0))
-  GrabCamera(L0_4, "demo_gv_isle3.CameraHut02", CAMERA_LERP_TRANSITION, 0.25)
+  SetTransformation("demo_gv_isle3.OswaldCameraControlPoint 01", vector4(90.9, 18.5, -13), vector4(0, 0, 0))
+  GrabCamera("demo_gv_isle3.OswaldCameraControlPoint 01", "demo_gv_isle3.CameraHut02", CAMERA_LERP_TRANSITION, 0.25)
   SetPropertyBool(GetPlayer(), "Invisible", true)
   ForcePainted("demo_gv_isle2.IGCBunnySpawner")
   Enable("demo_gv_isle2.IGCBunnySpawner")
@@ -50,7 +49,7 @@ function L0_0()
   GoToEntity("demo_gv_isle3.IGC_Spatter", "demo_gv_isle3.IGC_Eat_Spatter")
   wait(0.65)
   SetPropertyBool(GetPlayer(), "Invisible", true)
-  GrabCamera(L0_4, "demo_gv_isle3.IGC_BunnyChild", CAMERA_LERP_TRANSITION, 0.25)
+  GrabCamera("demo_gv_isle3.OswaldCameraControlPoint 01", "demo_gv_isle3.IGC_BunnyChild", CAMERA_LERP_TRANSITION, 0.25)
   Bark(GetPlayer(), "Well would you look at that.  Awwww, that cute Bunny child found some playmates.", 4, "Gus", "Gus:")
   wait(4.5)
   SetPropertyBool(GetPlayer(), "Invisible", true)
@@ -60,19 +59,16 @@ function L0_0()
   UnrestrictCutSceneInput()
   SetPropertyBool(GetPlayer(), "Invisible", false)
 end
-demo_gv_isle3_IGC_BunnyEatSpatter = L0_0
-L0_0 = 0
-demo_gv_isle2_IGC_Tower = L0_0
-function L0_0()
-  local L0_5
-  L0_5 = 1
-  demo_gv_isle2_IGC_Tower = L0_5
-  L0_5 = "demo_gv_isle3.OswaldCameraControlPoint 01"
+
+demo_gv_isle2_IGC_Tower = 0
+
+function demo_gv_isle3_IGC_TowerObj()
+  demo_gv_isle2_IGC_Tower = 1
   SetPropertyBool(GetPlayer(), "Invisible", true)
   RestrictCutSceneInput()
-  SetTransformation(L0_5, vector4(32.7, 38.6, -30.9), vector4(0, 0, 0))
+  SetTransformation("demo_gv_isle3.OswaldCameraControlPoint 01", vector4(32.7, 38.6, -30.9), vector4(0, 0, 0))
   wait(0.1)
-  GrabCamera(L0_5, "demo_gv_isle3.IGC_TowerLook", CAMERA_LERP_TRANSITION, 3)
+  GrabCamera("demo_gv_isle3.OswaldCameraControlPoint 01", "demo_gv_isle3.IGC_TowerLook", CAMERA_LERP_TRANSITION, 3)
   Bark(GetPlayer(), "Hmm, part of that tower is missing.  The other half must be on the other side of the island.", 2.75, "Gus", "Gus:")
   wait(3)
   Bark(GetPlayer(), "I imagine we can fix it and make our way across to the next island.", 2.75, "Gus", "Gus:")
@@ -84,13 +80,13 @@ function L0_0()
   UnrestrictCutSceneInput()
   SetPropertyBool(GetPlayer(), "Invisible", false)
 end
-demo_gv_isle3_IGC_TowerObj = L0_0
-function L0_0(A0_6, A1_7)
-  if A1_7 == "1" then
+
+function demo_gv_isle3_RemindObj(param1, param2)
+  if param2 == "1" then
     Bark(GetPlayer(), "Phew, we're almost there.", 3, "Gus", "Gus:")
-  elseif A1_7 == "2" then
+  elseif param2 == "2" then
   else
-    if A1_7 == "rand" then
+    if param2 == "rand" then
       if GetGlobal("demo_gv_isle3_mickeycheck2") == 1 then
         Bark(GetPlayer(), "Let's hurry to the other island, Mickey!", 2, "Gus", "Gus:")
       elseif GetGlobal("demo_gv_isle3_mickeycheck") == 1 then
@@ -101,34 +97,34 @@ function L0_0(A0_6, A1_7)
     end
   end
 end
-demo_gv_isle3_RemindObj = L0_0
-function L0_0(A0_8, A1_9, A2_10)
-  if GetGlobal(A1_9) == 0 then
+
+function demo_gv_isle3_bunnydoor(param1, param2, param3)
+  if GetGlobal(param2) == 0 then
     ForEachEntityInGroup(AI_SetDisabled, "demo_gv_isle1.SpatterGroup", true)
-    SetGlobal(A1_9, 1)
-    AnimGBSequence(A0_8, "DoorsClose")
-    GrabCamera(A2_10, GetPlayer(), CAMERA_LERP_TRANSITION, 1)
+    SetGlobal(param2, 1)
+    AnimGBSequence(param1, "DoorsClose")
+    GrabCamera(param3, GetPlayer(), CAMERA_LERP_TRANSITION, 1)
     wait(1)
-    AnimGBSequence(A0_8, "Zap")
+    AnimGBSequence(param1, "Zap")
     ShakeCamera_Default("camera")
     Rumble(GetPlayer(), 1.25)
     wait(2)
-    AnimGBSequence(A0_8, "DoorsOpen")
+    AnimGBSequence(param1, "DoorsOpen")
     ReleaseCamera(CAMERA_LERP_TRANSITION, 0.5)
     wait(2)
-    AnimGBSequence(A0_8, "Rest")
+    AnimGBSequence(param1, "Rest")
     ForEachEntityInGroup(AI_SetDisabled, "demo_gv_isle1.SpatterGroup", false)
   end
 end
-demo_gv_isle3_bunnydoor = L0_0
-function L0_0(A0_11)
+
+function demo_gv_isle3_KillBunny(param1)
   SetGlobal("demo_gv_isle3_totalbunnychildren", GetGlobal("demo_gv_isle3_totalbunnychildren") + 1)
   Bark(GetPlayer(), "That'll rid you of the bunnies. I wonder where those tubes come out?", 3, "Gus", "GUS:")
   AnimGBSequence("demo_gv_isle3.Demo_GV_BunnyBoxA1_Inert 02", "DoorsClose")
   GrabCamera("demo_gv_isle3.BunnyTubeCameraControlPoint 01", GetPlayer(), CAMERA_LERP_TRANSITION, 1)
   wait(1)
   AnimGBSequence("demo_gv_isle3.Demo_GV_BunnyBoxA1_Inert 02", "Zap")
-  DestroyEntity(A0_11)
+  DestroyEntity(param1)
   ShakeCamera_Default("camera")
   Rumble(GetPlayer(), 1.25)
   wait(2)
@@ -137,42 +133,39 @@ function L0_0(A0_11)
   wait(2)
   AnimGBSequence("demo_gv_isle3.Demo_GV_BunnyBoxA1_Inert 02", "Rest")
 end
-demo_gv_isle3_KillBunny = L0_0
-function L0_0()
+
+function demo_gv_isle3_bunnycheck()
   SetGlobal("demo_gv_isle3_bunnyhut", 0)
 end
-demo_gv_isle3_bunnycheck = L0_0
-function L0_0(A0_12)
-  local L1_13, L2_14, L3_15, L4_16
-  L1_13 = GetGlobal
-  L2_14 = "demo_gv_isle3U_TowerPosition"
-  L1_13 = L1_13(L2_14)
-  L2_14 = ""
-  L3_15 = ""
-  L4_16 = ""
+
+function demo_gv_isle2_freegremlin(param1)
+  local var1, var2, var3
+  var1 = ""
+  var2 = ""
+  var3 = ""
   RestrictCutSceneInput()
   ForEachEntityInGroup(AI_SetDisabled, "demo_gv.SpatterGroup", true)
-  if L1_13 == 0 then
-    L3_15 = "You honor me with your kindness. Allow me to stop that nearby fan!"
-    L2_14 = "demo_gv_isle3.OswaldCameraControlPoint 01"
-    SetTransformation(L2_14, vector4(96.6, 18.9, -36.3), vector4(0, 54.03763, 0))
-    L4_16 = "Hyperion"
+  if GetGlobal("demo_gv_isle3U_TowerPosition") == 0 then
+    var2 = "You honor me with your kindness. Allow me to stop that nearby fan!"
+    var1 = "demo_gv_isle3.OswaldCameraControlPoint 01"
+    SetTransformation(var1, vector4(96.6, 18.9, -36.3), vector4(0, 54.03763, 0))
+    var3 = "Hyperion"
   else
-    L3_15 = "Ten thousand thanks! There's a gramophone up top of this cliff that is driving me bonkers!"
-    L2_14 = "demo_gv_isle3.CameraFreeGremlin"
-    L4_16 = "Ivor"
+    var2 = "Ten thousand thanks! There's a gramophone up top of this cliff that is driving me bonkers!"
+    var1 = "demo_gv_isle3.CameraFreeGremlin"
+    var3 = "Ivor"
   end
-  GrabCamera(L2_14, A0_12, CAMERA_LERP_TRANSITION, 0)
+  GrabCamera(var1, param1, CAMERA_LERP_TRANSITION, 0)
   SetPropertyBool(GetPlayer(), "Invisible", true)
   wait(2)
-  AudioPostEventOn(A0_12, "Play_sfx_gremlin_hover")
-  EnterCutscene(A0_12)
-  AnimEvent(A0_12, EVENT_Start_Talk)
-  Bark(GetPlayer(), L3_15, 3, "GremlinBlue", L4_16)
+  AudioPostEventOn(param1, "Play_sfx_gremlin_hover")
+  EnterCutscene(param1)
+  AnimEvent(param1, EVENT_Start_Talk)
+  Bark(GetPlayer(), var2, 3, "GremlinBlue", var3)
   wait(1)
-  if L1_13 == 0 then
+  if GetGlobal("demo_gv_isle3U_TowerPosition") == 0 then
     wait(1.25)
-    GrabCamera(L2_14, "demo_gv_isle3.Demo_GV_FanA1_Inert 01", CAMERA_LERP_TRANSITION, 1)
+    GrabCamera(var1, "demo_gv_isle3.Demo_GV_FanA1_Inert 01", CAMERA_LERP_TRANSITION, 1)
     wait(1.25)
     ForEachEntityInGroup(SetRotatorMaxSpeed, "demo_gv_isle3.Demo_GV_FanA1_Inert 01", 0)
     FireUser1("demo_gv_isle3.Demo_GV_FanA1_Inert 01")
@@ -181,14 +174,14 @@ function L0_0(A0_12)
   else
   end
   wait(2)
-  DestroyEntity(A0_12)
+  DestroyEntity(param1)
   ReleaseCamera(CAMERA_LERP_TRANSITION, 0.5)
   UnrestrictCutSceneInput()
   SetPropertyBool(GetPlayer(), "Invisible", false)
   ForEachEntityInGroup(AI_SetDisabled, "demo_gv.SpatterGroup", false)
 end
-demo_gv_isle2_freegremlin = L0_0
-function L0_0(A0_17, A1_18)
+
+function demo_gv_isle3_towerfixed(param1, param2)
   if GetGlobal("demo_gv_isle3U_TowerPosition") == 0 then
     SetGlobal("demo_gv_isle3U_TowerPosition", 1)
     wait(0.1)
@@ -205,15 +198,15 @@ function L0_0(A0_17, A1_18)
   demo_gv_isle2_EnableSpawners("demo_gv_isle3.Demo_GV_SpatterSpawner 06", "demo_gv_isle3.Spawner 06")
   GoToEntity("demo_gv_isle3.Spatters_Island2", "demo_gv_isle3.Marker_Hut04")
   if 1 == 1 then
-    Enable(A0_17)
+    Enable(param1)
     wait(1)
     Bark(GetPlayer(), "Good job Mickey, now we can reach the Cannon up top of the tower.", 3, "Gus", "GUS:")
   elseif 1 == 2 then
-    DestroyEntity(A0_17)
+    DestroyEntity(param1)
     ForEachEntityInGroup(DestroyEntity, "demo_gv_isle3.PagodaCSGBottom")
     ForEachEntityInGroup(ForceSketched, "demo_gv_isle3.PagodaCSG_Bridge")
-    AnimGBReset(A1_18)
-    AnimGBSequence(A1_18, "RestDown")
+    AnimGBReset(param2)
+    AnimGBSequence(param2, "RestDown")
     ForEachEntityInGroup(Disable, "demo_gv_isle2.TowerCams")
     DestroyEntity("demo_gv_isle3.CannonFromTower")
     Unhide("demo_gv_isle2.FallenBridgeGear")
@@ -221,8 +214,8 @@ function L0_0(A0_17, A1_18)
     Bark(GetPlayer(), "I still can't believe you wrecked that tower, but let's just cross that bridge, Mickey.", 3, "Gus", "GUS:")
   end
 end
-demo_gv_isle3_towerfixed = L0_0
-function L0_0()
+
+function demo_gv_isle3_launchcutscene()
   RestrictCutSceneInput()
   Disable("demo_gv_isle3.Trigger_BridgeCollapse")
   MoveToEntity(GetPlayer(), "demo_gv_isle3.Isle4Marker")
@@ -241,38 +234,37 @@ function L0_0()
   wait(0.35)
   demo_gv_isle3_oswaldPeteIGC()
 end
-demo_gv_isle3_launchcutscene = L0_0
-function L0_0(A0_19)
+
+function demo_gv_isle2_bridgecollapse(param1)
   SetAutoSave("demo_gv_isle3.Isle4Marker 01")
   demo_gv_isle3_oswaldPeteIGC()
 end
-demo_gv_isle2_bridgecollapse = L0_0
-function L0_0()
+
+function demo_gv_isle2_bridgedrop()
   SetSplineFollowerInitialSpeed("demo_gv_isle2.Demo_GV_FallingBridgeA1_Inert 01", 30)
 end
-demo_gv_isle2_bridgedrop = L0_0
-function L0_0(A0_20)
+
+function demo_gv_isle2_bridgedrop2(param1)
   wait(0.9)
-  ClearParent(A0_20)
+  ClearParent(param1)
 end
-demo_gv_isle2_bridgedrop2 = L0_0
-function L0_0()
+
+function demo_gv_isle2b_KillCliffSpatters()
   if demo_gv_isl2_PlayerOnCliff == true then
     demo_gv_isl2_PlayerOnCliff = false
     ForEachEntityInGroup(DestroyEntity, "demo_gv_isle2b.SpattersCliff")
   end
 end
-demo_gv_isle2b_KillCliffSpatters = L0_0
-L0_0 = false
-demo_gv_isl2_PlayerOnCliff = L0_0
-function L0_0()
+
+demo_gv_isl2_PlayerOnCliff = false
+function demo_gv_isle2b_PlayerOnCliff()
   if demo_gv_isl2_PlayerOnCliff == false then
     demo_gv_isle2_EnableSpawners("demo_gv_isle3.Demo_GV_SpatterSpawner 03", "demo_gv_isle3.Spawner 03")
     demo_gv_isl2_PlayerOnCliff = true
   end
 end
-demo_gv_isle2b_PlayerOnCliff = L0_0
-function L0_0()
+
+function demo_gv_isle3_Underportal()
   if GetGlobal("demo_gv_isle3U_TowerPosition") == 0 then
     DialogBoxAdvanced("Enter Portal?", "Yes", "No")
     wait(0.25)
@@ -302,8 +294,8 @@ function L0_0()
     end
   end
 end
-demo_gv_isle3_Underportal = L0_0
-function L0_0()
+
+function demo_gv_isle3_UnderportalBottom()
   if GetGlobal("demo_gv_isle3U_TowerPosition") == 0 then
     DialogBoxAdvanced("Enter the Portal?", "Yes", "No")
     wait(0.25)
@@ -319,12 +311,10 @@ function L0_0()
     end
   end
 end
-demo_gv_isle3_UnderportalBottom = L0_0
-function L0_0()
-  local L0_21
-  L0_21 = "demo_gv_isle3_u.UpsideDownCameraControlPoint 01"
-  SetTransformation(L0_21, vector4(-52.2, 184.6, -37.5), vector4(0, -4.999993, 0))
-  GrabCamera(L0_21, "demo_gv_isle1.Demo_GV_PortalA1_Inert 01", CAMERA_LERP_TRANSITION, 1.5)
+
+function demo_gv_isle2_CamFlipExit()
+  SetTransformation("demo_gv_isle3_u.UpsideDownCameraControlPoint 01", vector4(-52.2, 184.6, -37.5), vector4(0, -4.999993, 0))
+  GrabCamera("demo_gv_isle3_u.UpsideDownCameraControlPoint 01", "demo_gv_isle1.Demo_GV_PortalA1_Inert 01", CAMERA_LERP_TRANSITION, 1.5)
   RestrictCutSceneInput()
   wait(1.5)
   UnrestrictCutSceneInput()
@@ -349,19 +339,19 @@ function L0_0()
   end
   SetGlobal("demo_gv_isle3_mickeycheck2", 1)
 end
-demo_gv_isle2_CamFlipExit = L0_0
-function L0_0(A0_22, A1_23)
-  if GetPropertyBool("demo_gv_isle3.GremlinHut0" .. A1_23, "Is Painted") then
-  elseif GetPropertyBool(A0_22, "TSCStartDisabled") then
-    AnimGBReset("demo_gv_isle3.Demo_GV_SpatterSpawner 0" .. A1_23)
-    AnimGBSequence("demo_gv_isle3.Demo_GV_SpatterSpawner 0" .. A1_23, "Appear")
+
+function demo_gv_isle2_hivecheck(param1, param2)
+  if GetPropertyBool("demo_gv_isle3.GremlinHut0" .. param2, "Is Painted") then
+  elseif GetPropertyBool(param1, "TSCStartDisabled") then
+    AnimGBReset("demo_gv_isle3.Demo_GV_SpatterSpawner 0" .. param2)
+    AnimGBSequence("demo_gv_isle3.Demo_GV_SpatterSpawner 0" .. param2, "Appear")
     wait(1)
-    Enable(A0_22)
+    Enable(param1)
   end
 end
-demo_gv_isle2_hivecheck = L0_0
-function L0_0(A0_24, A1_25)
-  if A1_25 == "start" then
+
+function Player_SetWatchVar(param1, param2)
+  if param2 == "start" then
     SetGlobal("Ability_WatchEnabled", 1)
     ForEachEntityInGroup(FireUser1, "ItemsAffectedByWatch")
   else
@@ -369,28 +359,27 @@ function L0_0(A0_24, A1_25)
     ForEachEntityInGroup(FireUser2, "ItemsAffectedByWatch")
   end
 end
-Player_SetWatchVar = L0_0
-function L0_0(A0_26, A1_27, A2_28)
+
+function demo_gv_isle2_CartoonFanCheck(param1, param2, param3)
   wait(0.1)
-  if A2_28 == "painted" then
-    ForcePainted(A0_26)
+  if param3 == "painted" then
+    ForcePainted(param1)
     if GetGlobal("Ability_WatchEnabled") == 0 then
-      Enable(A1_27)
+      Enable(param2)
     end
-  elseif A2_28 == "sketched" then
-    Disable(A1_27)
-  elseif GetPropertyBool(A0_26, "Is Painted") then
+  elseif param3 == "sketched" then
+    Disable(param2)
+  elseif GetPropertyBool(param1, "Is Painted") then
     if GetGlobal("Ability_WatchEnabled") == 1 then
-      Disable(A1_27)
+      Disable(param2)
     end
   else
-    Enable(A1_27)
+    Enable(param2)
   end
 end
-demo_gv_isle2_CartoonFanCheck = L0_0
-function L0_0()
+
+function demo_gv_isle2_inertfanbark()
   if GetPropertyBool("demo_gv_isle3.Spawner 07", "TSCStartDisabled") then
     Bark(GetPlayer(), "Hey Mickey, I imagine we could use the watch sketch to get past that fan.  Who knows?  Maybe there will be something useful inside.", 3, "Gus", "GUS:")
   end
 end
-demo_gv_isle2_inertfanbark = L0_0
--- Environments/_Shared/Scripts/Demo_GV_isle3U.lua
+++ Environments/_Shared/Scripts/Demo_GV_isle3U.lua
@@ -1,5 +1,6 @@
-local L0_0, L1_1
-function L0_0()
+-- Fully reworked
+
+function demo_gv_isle3u_upsidedowncamera()
   if GetGlobal("demo_gv_isle3U_FirstVisit") == 0 then
     RestrictCutSceneInput()
     SetConfigVariableString(GetPlayer(), "CameraUpsidedown", "true")
@@ -14,18 +15,12 @@ function L0_0()
     demo_gv_isle3U_GoalReveal()
   end
 end
-demo_gv_isle3u_upsidedowncamera = L0_0
-function L0_0()
-  local L0_2
-  L0_2 = GetGlobal
-  L0_2 = L0_2("demo_gv_isle3U_FirstVisit")
-  if L0_2 == 0 then
-    L0_2 = SetGlobal
-    L0_2("demo_gv_isle3U_FirstVisit", 1)
-    L0_2 = RestrictCutSceneInput
-    L0_2()
-    L0_2 = "demo_gv_isle3_u.UndersideGoalRevealCameraControlPoint 01"
-    GrabCamera(L0_2, "demo_gv_isle3_u.Demo_GV_BigBenA2_Inert 02", CAMERA_LERP_TRANSITION, 1.5)
+
+function demo_gv_isle3U_GoalReveal()
+  if GetGlobal("demo_gv_isle3U_FirstVisit") == 0 then
+    SetGlobal("demo_gv_isle3U_FirstVisit", 1)
+    RestrictCutSceneInput()
+    GrabCamera("demo_gv_isle3_u.UndersideGoalRevealCameraControlPoint 01", "demo_gv_isle3_u.Demo_GV_BigBenA2_Inert 02", CAMERA_LERP_TRANSITION, 1.5)
     wait(3)
     Bark(GetPlayer(), "Tsk...shoddy work on those tower supports.  They are just barely holding that tower in place. Better leave them alone or face the consequences!", 7, "Gus", "Gus:")
     GrabCamera("demo_gv_isle3_u.UndersideGoalRevealCameraControlPoint 07", "demo_gv_isle3_u.GVD_GearA2_Toon 04", CAMERA_LERP_TRANSITION, 0)
@@ -39,8 +34,8 @@ function L0_0()
     UnrestrictCutSceneInput()
   end
 end
-demo_gv_isle3U_GoalReveal = L0_0
-function L0_0(A0_3)
+
+function demo_gv_isle3U_SetupGremlin(param1)
   if GetGlobal("demo_gv_isle3U_TowerPosition") == 0 then
     AnimVarInt("demo_gv_isle3_u.Gremlin_Base 01", VAR_AI_Misc, 1)
     AnimEvent("demo_gv_isle3_u.Gremlin_Base 01", EVENT_AI_Misc)
@@ -48,21 +43,20 @@ function L0_0(A0_3)
     wait(1)
     DisableMotion("demo_gv_isle3_u.Gremlin_Base 01")
   else
-    DestroyEntity(A0_3)
+    DestroyEntity(param1)
     DestroyEntity("demo_gv_isle3_u.Gremlin_CageBottom")
     SetRotatorMaxSpeed("demo_gv_isle3_u.Demo_GV_BigBenA2_Inert 02", 400)
     Enable("demo_gv_isle3_u.Demo_GV_BigBenA2_Inert 02")
   end
 end
-demo_gv_isle3U_SetupGremlin = L0_0
-function L0_0()
-  local L0_4
-  L0_4 = "demo_gv_isle3_u.Gremlin_Base 01"
+
+function demo_gv_isle3U_GremlinFreed()
+  local var1 = "demo_gv_isle3_u.Gremlin_Base 01"
   GrabCamera("demo_gv_isle3_u.TowerRestoreCameraControlPoint 01", nil, CAMERA_LERP_TRANSITION, 0)
   MoveToEntity(GetPlayer(), "demo_gv_isle3_u.MickeyAtGremlin")
   wait(0.25)
   RestrictCutSceneInput()
-  AnimEvent(L0_4, EVENT_Start_Talk)
+  AnimEvent(var1, EVENT_Start_Talk)
   if GetGlobal("demo_gv_isle3U_TowerPosition") == 0 then
     SetGlobal("demo_gv_isle3U_TowerPosition", 1)
     wait(0.5)
@@ -72,29 +66,29 @@ function L0_0()
     AnimEvent("demo_gv_isle3_u.Gremlin_Base 01", EVENT_Start_EmoteHappy)
     wait(2.5)
     GrabCamera("demo_gv_isle3_u.TowerRestoreCameraControlPoint 01", "demo_gv_isle3_u.Demo_GV_BigBenA2_Inert 02", CAMERA_LERP_TRANSITION, 0.75)
-    SetVisualScale(L0_4, 0.3)
-    AnimVarInt(L0_4, VAR_Cutscene, 1)
-    AnimEvent(L0_4, EVENT_Start_Cutscene)
-    SetTransformation(L0_4, vector4(-39.2, 181.4, -63.1), vector4(0, -80, 0))
+    SetVisualScale(var1, 0.3)
+    AnimVarInt(var1, VAR_Cutscene, 1)
+    AnimEvent(var1, EVENT_Start_Cutscene)
+    SetTransformation(var1, vector4(-39.2, 181.4, -63.1), vector4(0, -80, 0))
     wait(0.75)
-    SetTransformation(L0_4, vector4(-39.5, 191.6, -59.8), vector4(-180, -50, -180))
+    SetTransformation(var1, vector4(-39.5, 191.6, -59.8), vector4(-180, -50, -180))
     wait(0.75)
-    SetTransformation(L0_4, vector4(-37.1, 190.3, -64.3), vector4(0, -50, 0))
+    SetTransformation(var1, vector4(-37.1, 190.3, -64.3), vector4(0, -50, 0))
     wait(1)
-    SetTransformation(L0_4, vector4(-39.3, 196, -61.7), vector4(0, -90, 0))
+    SetTransformation(var1, vector4(-39.3, 196, -61.7), vector4(0, -90, 0))
     Bark(GetPlayer(), "How helpful! He's fixing the tower so we can reach the cannon at the top. Wow! See that detail? Did some of it myself!", 5, "Gus", "Gus:")
     wait(0.75)
-    SetTransformation(L0_4, vector4(-39.7, 185.4, -60.4), vector4(0, -80, 0))
+    SetTransformation(var1, vector4(-39.7, 185.4, -60.4), vector4(0, -80, 0))
     wait(0.75)
-    SetTransformation(L0_4, vector4(-39, 191.4, -58.6), vector4(-180, -40, -180))
+    SetTransformation(var1, vector4(-39, 191.4, -58.6), vector4(-180, -40, -180))
     wait(0.75)
-    SetTransformation(L0_4, vector4(-39.5, 191.6, -59.8), vector4(-180, -50, -180))
+    SetTransformation(var1, vector4(-39.5, 191.6, -59.8), vector4(-180, -50, -180))
     wait(0.75)
-    SetTransformation(L0_4, vector4(-37.1, 190.3, -64.3), vector4(0, -50, 0))
+    SetTransformation(var1, vector4(-37.1, 190.3, -64.3), vector4(0, -50, 0))
     wait(0.75)
-    SetTransformation(L0_4, vector4(-39.2, 181.4, -63.1), vector4(0, -80, 0))
+    SetTransformation(var1, vector4(-39.2, 181.4, -63.1), vector4(0, -80, 0))
     wait(0.75)
-    SetTransformation(L0_4, vector4(0, 0, 0), vector4(0, -50, 0))
+    SetTransformation(var1, vector4(0, 0, 0), vector4(0, -50, 0))
     Enable("demo_gv_isle3_u.Demo_GV_BigBenA2_Inert 02")
     SetRotatorMaxSpeed("demo_gv_isle3_u.Demo_GV_BigBenA2_Inert 02", 40)
     ForEachEntityInGroup(Enable, "GearCovers")
@@ -130,12 +124,12 @@ function L0_0()
   end
   UnrestrictCutSceneInput()
 end
-demo_gv_isle3U_GremlinFreed = L0_0
-function L0_0(A0_5, A1_6)
-  if GetGlobal(A1_6) == 0 then
-    SetGlobal(A1_6, 1)
-  elseif GetGlobal(A1_6) == 1 then
-    SetGlobal(A1_6, 0)
+
+function demo_gv_isle3U_Gear(param1, param2)
+  if GetGlobal(param2) == 0 then
+    SetGlobal(param2, 1)
+  elseif GetGlobal(param2) == 1 then
+    SetGlobal(param2, 0)
   end
   if GetGlobal("demo_gv_isle3U_Gear2Thinnedglobal") == 1 and GetGlobal("demo_gv_isle3U_Gear1Thinnedglobal") == 1 and GetGlobal("demo_gv_isle3U_TowerPosition") == 0 then
     SetPropertyBool(GetPlayer(), "Invisible", true)
@@ -179,16 +173,16 @@ function L0_0(A0_5, A1_6)
     AudioPostEventOn("demo_gv_isle1.Demo_GV_PortalA1_Inert 01", "Play_sfx_projector_portal")
   end
 end
-demo_gv_isle3U_Gear = L0_0
-function L0_0()
+
+function demo_gv_isle3u_startplatforms()
   ForEachEntityInGroup(SetRotatorMaxSpeed, "demo_gv_isle3_u.PlatformGroup1", 150)
 end
-demo_gv_isle3u_startplatforms = L0_0
-function L0_0()
+
+function demo_gv_isle3u_endplatforms()
   ForEachEntityInGroup(SetRotatorMaxSpeed, "demo_gv_isle3_u.PlatformGroup1", 0)
 end
-demo_gv_isle3u_endplatforms = L0_0
-function L0_0()
+
+function AreaLevelStreamCheck()
   StreamInLevel(GetPlayer(), "Environments/Demo/GSA/Demo_GV_Isle4.gsa")
   Wait(0.5)
   StreamInLevel(GetPlayer(), "Environments/Demo/GSA/Demo_GV_Isle3.gsa")
@@ -196,79 +190,60 @@ function L0_0()
   WaitForLevelLoad(GetPlayer(), "Environments/Demo/GSA/Demo_GV_Isle3.gsa")
   MoveToEntity(GetPlayer(), "demo_gv_gear_transition_area2.Marker(Rotatable) 04")
 end
-AreaLevelStreamCheck = L0_0
-L0_0 = 0
-gv_isle3u_roboticarmposition = L0_0
-function L0_0()
-  local L0_7, L1_8
-  L0_7 = gv_isle3u_roboticarmposition
-  L0_7 = L0_7 + 1
-  gv_isle3u_roboticarmposition = L0_7
-  L0_7 = gv_isle3u_roboticarmposition
-  if L0_7 == 2 then
-    L0_7 = 0
-    gv_isle3u_roboticarmposition = L0_7
+
+gv_isle3u_roboticarmposition = 0
+
+function demo_gv_isle3u_roboticarmpos()
+  gv_isle3u_roboticarmposition = gv_isle3u_roboticarmposition + 1
+  if gv_isle3u_roboticarmposition == 2 then
+    gv_isle3u_roboticarmposition = 0
   end
 end
-demo_gv_isle3u_roboticarmpos = L0_0
-function L0_0(A0_9)
+
+function demo_gv_isle3_maxspeed(param1)
   if GetGlobal("demo_gv_isle3U_Gremlinsaved") == 0 then
-    AnimGBReset(A0_9)
-    AnimGBSequence(A0_9, "Flip")
-    FireUser1(A0_9)
-    AudioStopPlayingOn(A0_9)
-    AudioPostEventOn(A0_9, "Play_sfx_gv_arm_over")
+    AnimGBReset(param1)
+    AnimGBSequence(param1, "Flip")
+    FireUser1(param1)
+    AudioStopPlayingOn(param1)
+    AudioPostEventOn(param1, "Play_sfx_gv_arm_over")
   end
 end
-demo_gv_isle3_maxspeed = L0_0
-function L0_0(A0_10)
-  SetRotatorMaxSpeed(A0_10, 0)
+
+function demo_gv_isle3_stopspeed(param1)
+  SetRotatorMaxSpeed(param1, 0)
 end
-demo_gv_isle3_stopspeed = L0_0
-function L0_0(A0_11)
+
+function demo_gv_isle3_maxspeedTHROW(param1)
   if GetGlobal("demo_gv_isle3U_Gremlinsaved") == 0 then
-    AnimGBSequence(A0_11, "Throw")
+    AnimGBSequence(param1, "Throw")
   end
 end
-demo_gv_isle3_maxspeedTHROW = L0_0
-function L0_0(A0_12)
-  local L1_13
-  L1_13 = Wait
-  L1_13(0.12)
-  L1_13 = CSG_GetCompletionFraction
-  L1_13 = L1_13(A0_12)
-  SplineFollower_StopAtPosition("BeetleworxArm1", "BeetleworxArm1StartKnot", "BeetleworxArm1EndKnot", L1_13)
+
+function CSGBeetleworxArm1(param1)
+  Wait(0.12)
+  SplineFollower_StopAtPosition("BeetleworxArm1", "BeetleworxArm1StartKnot", "BeetleworxArm1EndKnot", CSG_GetCompletionFraction(param1))
   SplineFollower_StopAtPosition("BeetleworxArm1Weight", "BeetleworxArm1WeightStartKnot", "BeetleworxArm1WeightEndKnot", L1_13)
 end
-CSGBeetleworxArm1 = L0_0
-function L0_0(A0_14)
-  local L1_15
-  L1_15 = Wait
-  L1_15(0.12)
-  L1_15 = CSG_GetCompletionFraction
-  L1_15 = L1_15(A0_14)
-  SplineFollower_StopAtPosition("BeetleworxArm2", "BeetleworxArm2StartKnot", "BeetleworxArm2EndKnot", L1_15)
+
+function CSGBeetleworxArm2(param1)
+  Wait(0.12)
+  SplineFollower_StopAtPosition("BeetleworxArm2", "BeetleworxArm2StartKnot", "BeetleworxArm2EndKnot", CSG_GetCompletionFraction(param1))
   SplineFollower_StopAtPosition("BeetleworxArm2Weight", "BeetleworxArm2WeightStartKnot", "BeetleworxArm2WeightEndKnot", L1_15)
 end
-CSGBeetleworxArm2 = L0_0
-function L0_0(A0_16)
-  local L1_17
-  L1_17 = Wait
-  L1_17(0.12)
-  L1_17 = CSG_GetCompletionFraction
-  L1_17 = L1_17(A0_16)
-  SplineFollower_StopAtPosition("BeetleworxArm3", "BeetleworxArm3StartKnot", "BeetleworxArm3EndKnot", L1_17)
+
+function CSGBeetleworxArm3(param1)
+  Wait(0.12)
+  SplineFollower_StopAtPosition("BeetleworxArm3", "BeetleworxArm3StartKnot", "BeetleworxArm3EndKnot", CSG_GetCompletionFraction(param1))
   SplineFollower_StopAtPosition("BeetleworxArm3Weight", "BeetleworxArm3WeightStartKnot", "BeetleworxArm3WeightEndKnot", L1_17)
 end
-CSGBeetleworxArm3 = L0_0
-function L0_0(A0_18)
+
+function demo_gv_isle3_cave(param1)
   Bark(GetPlayer(), "Let's go back through the filmstrip, Mickey!", 5, "Gus", "Gus:")
 end
-demo_gv_isle3_cave = L0_0
-function L0_0()
-  local L0_19
-  L0_19 = "demo_gv_isle3_u.UpsideDownCameraControlPoint 02"
-  GrabCamera(L0_19, "demo_gv_isle1.Demo_GV_PortalA1_Inert 01", CAMERA_LERP_TRANSITION, 1.5)
+
+function demo_gv_isle2_CamFlipExit2()
+  GrabCamera("demo_gv_isle3_u.UpsideDownCameraControlPoint 02", "demo_gv_isle1.Demo_GV_PortalA1_Inert 01", CAMERA_LERP_TRANSITION, 1.5)
   RestrictCutSceneInput()
   wait(1.5)
   UnrestrictCutSceneInput()
@@ -293,19 +268,19 @@ function L0_0()
   end
   SetGlobal("demo_gv_isle3_mickeycheck2", 1)
 end
-demo_gv_isle2_CamFlipExit2 = L0_0
-function L0_0()
+
+function demo_gv_isle2_PortalUsable0()
   SetPropertyFloat("demo_gv_isle1.Demo_GV_PortalA1_Inert 01", "UsableRadius", 0)
 end
-demo_gv_isle2_PortalUsable0 = L0_0
-function L0_0()
+
+function demo_gv_isle2_startupgoal()
   PauseMenuObjectives(GetPlayer(), [[
 Goal: 
  Fix or Topple the Tower to Open the 
  Passage to the Next Island]])
 end
-demo_gv_isle2_startupgoal = L0_0
-function L0_0(A0_20)
+
+function demo_gv_isle3_EnterportalTop(param1)
   RestrictCutSceneInput()
   if GetGlobal("Gremlinfreed") == 0 then
     Bark(GetPlayer(), "Are you going to enter the portal and just leave Cicero caged like that?", 5, "Gus", "GUS:")
@@ -332,4 +307,3 @@ function L0_0(A0_20)
     UnrestrictCutSceneInput()
   end
 end
-demo_gv_isle3_EnterportalTop = L0_0
